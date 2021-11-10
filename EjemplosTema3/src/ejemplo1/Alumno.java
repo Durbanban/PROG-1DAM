1e3a6c7e591a6cfcc9703d988942321472cc8115
Carlos Jesús Durbán Viloca <carlosdurban91@gmail.com>
2021-11-10 11:37:44 +0100

    Clase 10/11/2021

--- EjemplosTema3/src/ejemplo1/Alumno.java
+++ EjemplosTema3/src/ejemplo1/Alumno.java
@@ -4,10 +4,10 @@ public class Alumno {
 	
 	// Atributos
 	
-	int edad;
-	String nombre;
-	String apellidos;
-	double notaMedia;
+	private int edad;
+	private String nombre;
+	private String apellidos;
+	private double notaMedia;
 	
 	//Constructor
 	
@@ -17,5 +17,49 @@ public class Alumno {
 		this.apellidos = apellidos;
 		this.notaMedia = notaMedia;
 	}
+	
+	//Constructor sin edad
+	
+	public Alumno (String nombre, String apellidos, double notaMedia) {
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.notaMedia = notaMedia;
+	}
+	
+		
+	//getters and setters
+	
+	public int getEdad () {
+		return edad;
+	}
+	public void setEdad (int edad) {
+		this.edad = edad;
+	}
+	public String getNombre () {
+		return nombre;
+	}
+	public void setNombre (String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellidos () {
+		return apellidos;
+	}
+	public void setApellidos (String apellidos) {
+		this.apellidos = apellidos;
+	}
+	public double getNotaMedia () {
+		return notaMedia;
+	}
+	public void setNotaMedia (double notaMedia) {
+		this.notaMedia = notaMedia;
+	}
+	
+	//Método nuestro
+	
+	public double sumarPuntos (double puntos) {
+		notaMedia = notaMedia + puntos;
+		return notaMedia;
+	}
+
 
 }
--- EjemplosTema3/src/ejemplo1/Principal.java
+++ EjemplosTema3/src/ejemplo1/Principal.java
@@ -8,7 +8,7 @@ public class Principal {
 		// TODO Auto-generated method stub
 		int edad = 0;
 		String nombre, apellidos;
-		double notaMedia = 0.0;
+		double notaMedia = 0.0, puntos = 0.0;
 		Alumno a1 = new Alumno (30, "Carlos Jesús", "Durbán Viloca", 9.3);
 		Alumno a2 = new Alumno (25, "Álvaro", "Franco Martínez", 9.2);
 		Alumno aEditado;
@@ -22,21 +22,41 @@ public class Principal {
 		System.out.println("Introduzca la nota media");
 		notaMedia = Leer.datoDouble();
 		aEditado = new Alumno (edad, nombre, apellidos, notaMedia);
-		System.out.println("Mi edad es " + a1.edad);
-		System.out.println("Mi nombre es " + a1.nombre);
-		System.out.println("Mis apellidos son " + a1.apellidos);
-		System.out.println("Mi nota media es " + a1.notaMedia);
-		System.out.println("Mi edad es " + a2.edad);
-		System.out.println("Mi nombre es " + a2.nombre);
-		System.out.println("Mis apellidos son " + a2.apellidos);
-		System.out.println("Mi nota media es " + a2.notaMedia);
-		System.out.println("Mi edad es " + aEditado.edad);
-		System.out.println("Mi nombre es " + aEditado.nombre);
-		System.out.println("Mis apellidos son " + aEditado.apellidos);
-		System.out.println("Mi nota media es " + aEditado.notaMedia);
+		System.out.println("Mi edad es " + a1.getEdad ());
+		System.out.println("Mi nombre es " + a1.getNombre ());
+		System.out.println("Mis apellidos son " + a1.getApellidos ());
+		System.out.println("Mi nota media es " + a1.getNotaMedia ());
+		System.out.println("Mi edad es " + a2.getEdad ());
+		System.out.println("Mi nombre es " + a2.getNombre ());
+		System.out.println("Mis apellidos son " + a2.getApellidos ());
+		System.out.println("Mi nota media es " + a2.getNotaMedia ());
+		System.out.println("Mi edad es " + aEditado.getEdad ());
+		System.out.println("Mi nombre es " + aEditado.getNombre ());
+		System.out.println("Mis apellidos son " + aEditado.getApellidos() );
+		System.out.println("Mi nota media es " + aEditado.getNotaMedia ());
 		System.out.println(a1);
 		System.out.println(a2);
 		System.out.println(aEditado);
+		
+		System.out.println("Introduzca una nueva nota media: ");
+		notaMedia = Leer.datoDouble();
+		aEditado.setNotaMedia(notaMedia);
+		System.out.println("Su nueva nota media es " + aEditado.getNotaMedia ());
+		
+		System.out.println("Introduzca una nueva nota media de Carlos Jesús: ");
+		notaMedia = Leer.datoDouble();
+		a1.setNotaMedia (notaMedia);
+		System.out.println("La nueva nota media de Carlos Jesús es: " + a1.getNotaMedia ());
+		System.out.println("Mi edad es " + a1.getEdad ());
+		System.out.println("Mi nombre es " + a1.getNombre ());
+		System.out.println("Mis apellidos son " + a1.getApellidos ());
+		System.out.println("Mi nota media es " + a1.getNotaMedia ());
+		
+		System.out.println("¿Cuántos puntos quiere sumar a la nota media?");
+		puntos = Leer.datoDouble();
+		aEditado.sumarPuntos(puntos);
+		System.out.println("Su nueva nota media es: " + aEditado.getNotaMedia ());
+		
 	}
 
 }
