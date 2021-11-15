12a1a2b098bee8c199c1f1163f95af7ee3996a8c
Carlos Jesús Durbán Viloca <carlosdurban91@gmail.com>
2021-11-15 13:39:47 +0100

    Falta terminar el ejercicio05

---
+++ EjerT3List1CarlosDurban/src/ejercicio05/Sorteo.java
@@ -0,0 +1,44 @@
+package ejercicio05;
+import java.util.Random;
+public class Sorteo {
+	// Constructor vacío
+	public Sorteo () {
+		
+	}
+	
+	public int calcularNum () {
+		int valor = 0;
+		int max = 9;
+		int min = 0;
+		Random num = new Random (System.nanoTime());
+		valor = num.nextInt(max - min + 1) + min;
+		return valor;
+	}
+	public char calcularQuiniela () {
+		char valor;
+		int prob = 0, max = 3, min = 1;
+		Random num = new Random (System.nanoTime());
+		prob = num.nextInt(max - min + 1) + min;
+		if (prob > 2) {
+			valor = '1';
+			return valor;
+		}else if (prob == 2) {
+			valor = 'X';
+			return valor;
+		}else {
+			valor = '2';
+			return valor;
+		}
+
+	}
+	public String calcularParidad () {
+		
+		String resultado = "";
+		if (num % 2 == 0) {
+			resultado = "par";
+		}else {
+			resultado = "impar";
+		}
+		return resultado;
+	}
+}
