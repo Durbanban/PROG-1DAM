b6728b3f7a4bfe80384495bfb4af142e0fa71097
Carlos Jesús Durbán Viloca <carlosdurban91@gmail.com>
2022-01-12 10:50:18 +0100

    El ejercicio 9 está apuntito

--- EjerT3List1CarlosDurban/src/ejercicio09/Maquina.java
+++ EjerT3List1CarlosDurban/src/ejercicio09/Maquina.java
@@ -3,9 +3,11 @@ package ejercicio09;
 public class Maquina {
 	
 	private String password;
+	private double recaudacion;
 	
-	public Maquina (String password) {
+	public Maquina (String password, double recaudacion) {
 		this.password = password;
+		this.recaudacion = recaudacion;
 	}
 		
 	public String getPassword() {
@@ -15,31 +17,52 @@ public class Maquina {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	public double getRecaudacion () {
+		return recaudacion;
+	}
+	
+	public void setRecaudacion (double recaudacion) {
+		this.recaudacion = recaudacion;
+	}
 
 
-
-	public double comprarBilletes (int numBilletes, double cantInsertada, double precioTicket) {	
-		double coste = 0.0, devolucion = 0.0;
-		coste = numBilletes * precioTicket;
-		devolucion = cantInsertada - coste;
-		return devolucion;
+	public boolean comprobarCompra (int numBilletes, double cantInsertada, Ticket t) {	
+		double coste = 0.0;
+		coste = numBilletes * t.getPrecio();
+		if (coste <= cantInsertada) {
+			return false;
+		}else {
+			return true;
+		}
+	}
+	
+	public double devolverImporte (int numBilletes, double cantInsertada, Ticket t) {
+		double res = 0.0, coste = 0.0;
+		coste = numBilletes * t.getPrecio();
+		res = cantInsertada - coste;
+		this.recaudacion = recaudacion + coste;
+		return res;
 	}
 	
 	public boolean pedirPassword (String pass) {		
-		boolean confirmacion;
 		if (pass.equals(getPassword())) {
-			confirmacion = false;
+			return false;
 		}else {
-			confirmacion = true;
+			return true;
 		}
-		return confirmacion;
 	}
 	
-	public void bienvenida () {
-		System.out.println("Bienvenido al metro de Triana");
+	public void menuPrincipal () {
 		System.out.println("Pulse 1 para comprar billetes");
 		System.out.println("Pulse 2 para opciones de operario");
 		System.out.println("Pulse 0 para salir");
+	}
+	
+	public void menuOperario () {
+		System.out.println("Pulse 1 para mostrar recaudación");
+		System.out.println("Pulse 2 para resetear la recaudación");
+		System.out.println("Pulse 0 para volver al menú principal");
 	}
 	
 	
--- EjerT3List1CarlosDurban/src/ejercicio09/Principal.java
+++ EjerT3List1CarlosDurban/src/ejercicio09/Principal.java
@@ -7,30 +7,64 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String  defPassword = "123", pass;
+		String  pass;
 		Ticket ticket = new Ticket (1.5);
-		Maquina maquina = new Maquina (defPassword);
+		Maquina maquina = new Maquina ("123", 0.0);
 		double cantInsertada = 0.0;
-		int opcion = 0, numBillete = 0;
+		int opcion = 0, numBillete = 0, opcionOperario = 0;
+		boolean check;
 
-		maquina.bienvenida();
-		opcion = Leer.datoInt();
-		switch (opcion) {
-			case 1:
-				numBillete = Leer.datoInt();
-				cantInsertada = Leer.datoDouble();
-				System.out.println(maquina.comprarBilletes(numBillete, cantInsertada, ticket.getPrecio()));
-			case 2:
-				System.out.println("Introduzca la contraseña de operario");
-				pass = Leer.dato();
-				while (maquina.pedirPassword(pass)) {
-					System.out.println("Contraseña incorrecta, vuelva a intentarlo");
-					pass = Leer.dato();
-				}
+		System.out.println("Bienvenido al metro de Triana");
+		do {
+			maquina.menuPrincipal();
+			opcion = Leer.datoInt();
+			switch (opcion) {
+				case 1:
+					System.out.println("¿Cuántos billetes quiere?");
+					numBillete = Leer.datoInt();
+					System.out.println("¿Qué cantidad va a introducir?");
+					cantInsertada = Leer.datoDouble();
+					check = maquina.comprobarCompra(numBillete, cantInsertada, ticket);
+					while (check) {
+						System.out.println("Lo sentimos, la cantidad insertada no es suficiente");
+						System.out.println("¿Cuántos billetes quiere?");
+						numBillete = Leer.datoInt();
+						System.out.println("¿Qué cantidad va a introducir?");
+						cantInsertada = Leer.datoDouble();
+						check = maquina.comprobarCompra(numBillete, cantInsertada, ticket);
+					}
+					System.out.println("Su cambio es: " + maquina.devolverImporte(numBillete, cantInsertada, ticket) + " €.");
 					
-				
-				
-		}
+					break;
+				case 2:
+					System.out.println("Introduzca la contraseña para operarios");
+					pass = Leer.dato();
+					while (maquina.pedirPassword(pass)) {
+						System.out.println("Contraseña incorrecta, vuelva a intentarlo");
+						pass = Leer.dato();
+					}
+					maquina.menuOperario();
+					opcionOperario = Leer.datoInt();
+					switch (opcionOperario) {
+						case 1:
+							System.out.println("El total de hoy es: " + maquina.getRecaudacion() + " €.");
+							break;
+						case 2:
+							maquina.setRecaudacion(0.0);
+							break;
+						case 0:
+							break;
+						default:
+							System.out.println("Elija una opción correcta");
+							break;
+					}
+					break;
+					
+						
+					
+					
+			}
+		}while (opcion != 0);
 
 	}
 
