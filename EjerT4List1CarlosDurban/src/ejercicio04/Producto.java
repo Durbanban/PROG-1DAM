eac68e13f4300afaaa887aecb4d18ac3134ebc94
Durbanban <carlosdurban91@gmail.com>
2022-02-06 19:55:18 +0100

    Empezado el ejercicio 4

---
+++ EjerT4List1CarlosDurban/src/ejercicio04/Producto.java
@@ -0,0 +1,54 @@
+package ejercicio04;
+
+public class Producto {
+	
+	private double precioUnitario;
+	private String nombre;
+	private String id;
+	
+	
+	public Producto (double precioUnitario, String nombre, String id) {
+		this.precioUnitario = precioUnitario;
+		this.nombre = nombre;
+		this.id = id;
+	}
+
+
+	public double getPrecioUnitario() {
+		return precioUnitario;
+	}
+
+
+	public void setPrecioUnitario(double precioUnitario) {
+		this.precioUnitario = precioUnitario;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getId() {
+		return id;
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Producto [precioUnitario=" + precioUnitario + ", nombre=" + nombre + ", id=" + id + "]";
+	}
+	
+	
+
+}
