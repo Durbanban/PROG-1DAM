568cfe059e5bdc66c922c1d60c915da212d26165
Carlos Jesús Durbán Viloca <carlosdurban91@gmail.com>
2022-02-22 14:26:09 +0100

    Ejemplo de HashSet añadido

--- EjemplosTema5/src/ejemploArrayList/Principal.java
+++ EjemplosTema5/src/ejemploArrayList/Principal.java
@@ -1,6 +1,7 @@
 package ejemploArrayList;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Stack;
 
@@ -13,11 +14,38 @@ public class Principal {
 		List <String> lista = new ArrayList <String> ();
 		List <String []> listaArrays = new ArrayList <String []> ();
 		List <String> pila = new Stack <String> ();
-		String [] nombres = new String [tam];
+		String [] nombres = {"pollo", "pollo2", "pollo3"};
+		List <Alumno> listaAlumnos = new ArrayList <Alumno> ();
+		
+		Alumno a = new Alumno ("Miguel", 37, 4.7);
+		Alumno a2 = new Alumno ("Álvaro", 42, 5.4);
 		
 		lista.add("Durbán");
 		lista.add("Álvaro");
 		System.out.println(lista);
+		System.out.println(lista.get(1));
+		lista.remove(0);
+		System.out.println(lista.get(0));
+		lista.clear();
+		System.out.println(lista);
+		listaArrays.add(nombres);
+		System.out.println(listaArrays.get(0)[1]);
+		listaAlumnos.add(0, a2);
+		listaAlumnos.add(new Alumno ("Durbán", 30, 9.2));
+		listaAlumnos.add(0, a);
+		System.out.println(listaAlumnos);
+		System.out.println(listaAlumnos.size());
+		for (int i = 0; i < listaAlumnos.size(); i++) {
+			System.out.println(listaAlumnos.get(i).getEdad() + " años");
+		}
+		for (Alumno alumno : listaAlumnos) {
+			if (alumno.getNotaMedia() > 5) {
+				System.out.println(alumno);
+			}else {
+				System.out.println("Suspendido");
+			}
+		}
+		
 	}
 
 }
---
+++ EjemplosTema5/src/ejemploSet/Principal.java
@@ -0,0 +1,21 @@
+package ejemploSet;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Set <String> lista = new HashSet <String> ();
+		
+		lista.add("Durbán");
+		lista.add("Álvaro");
+		lista.add("Ángel");
+		lista.add("Miguel");
+		System.out.println(lista);
+
+	}
+
+}
