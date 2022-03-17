f7a643739434fa5f302d15f6b9661c4eee63fecd
Carlos Jesús Durbán Viloca <carlosdurban91@gmail.com>
2022-03-17 14:26:58 +0100

    lambdaasdasdasdsa

--- EjerT5List1CarlosDurban/src/ejercicio06/Principal.java
+++ EjerT5List1CarlosDurban/src/ejercicio06/Principal.java
@@ -2,7 +2,10 @@ package ejercicio06;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
 
 import lectura.Leer;
 
@@ -13,6 +16,17 @@ public class Principal {
 		int idTrabajador = 1, opcionMenu = 0;
 		List <Trabajador> listaTrabajadores = new ArrayList <Trabajador> ();
 		
+		BiFunction <Trabajador, Trabajador, Integer> bifuncion = (t1, t2) -> {
+			int res = 0;
+			if (t1.getHorasTrabajadas() > t2.getHorasTrabajadas()) {
+				res = -1;
+			}else {
+				if (t1.getHorasTrabajadas() < t2.getHorasTrabajadas()) {
+					res = 1;
+				}
+			}
+			return res;
+		};
 		Trabajador t1 = new Trabajador (idTrabajador ++, "Pepito Grillo", "55255469-F", 25, 11.50);
 		Trabajador t2 = new Trabajador (idTrabajador ++, "Shrek", "77474105-H", 38, 8.50);
 		Trabajador t3 = new Trabajador (idTrabajador ++, "Cenicienta", "44510235-I", 2, 65.0);
@@ -46,35 +60,74 @@ public class Principal {
 					crudTrabajador.mostrarLista();
 					break;
 				case 3:
-					crudTrabajador.getListado().sort((o1, o2) -> {
-						int res = 0;
-						if (t1.getHorasTrabajadas() > t2.getHorasTrabajadas()) {
-							res = -1;
-						}else {
-							if (t1.getHorasTrabajadas() < t2.getHorasTrabajadas()) {
-								res = 1;
-							}
-						}
-						return res;
-					});
+//					Collections.sort(crudTrabajador.getListado(), bifuncion.apply(t1, t2));
 					
-					crudTrabajador.getListado().sort(o1, o2 -> {
-						int res = 0;
-						if (t1.getHorasTrabajadas() > t2.getHorasTrabajadas()) {
-							res = -1;
-						}else if (t1.getHorasTrabajadas() < t2.getHorasTrabajadas()) {
-							res = 1;
-						}else {
-							res = 0;
-						}
-						return res;
-					});
+//					crudTrabajador.getListado().sort((o1, o2) -> {
+//						int res = 0;
+//						if (t1.getHorasTrabajadas() > t2.getHorasTrabajadas()) {
+//							res = -1;
+//						}else if (t1.getHorasTrabajadas() < t2.getHorasTrabajadas()) {
+//							res = 1;
+//						}else {
+//							res = 0;
+//						}
+//						return res;
+//					});
 					//crudTrabajador.getListado().sort(cph);
-					//Collections.sort(crudTrabajador.getListado(), cph);
+					Collections.sort(crudTrabajador.getListado(), new Comparator <Trabajador>() {
+
+						@Override
+						public int compare(Trabajador o1, Trabajador o2) {
+							int res = 0;
+							if (o1.getHorasTrabajadas() > o2.getHorasTrabajadas()) {
+								res = -1;
+							}else if (o1.getHorasTrabajadas() < o2.getHorasTrabajadas()) {
+								res = 1;
+							}else {
+								res = 0;
+							}
+							return res;
+						}
+						
+					});
 					crudTrabajador.mostrarLista();
 					break;
 				case 4:
-					Collections.sort(crudTrabajador.getListado(), cps);
+					
+//					crudTrabajador.getListado().sort((o1, o2) -> {
+//						// TODO Auto-generated method stub
+//						int res = 0;
+//						if (t1.calcularSueldo() > t2.calcularSueldo()) {
+//							res = 1;
+//						}else {
+//							if (t1.calcularSueldo() < t2.calcularSueldo()) {
+//								res = -1;
+//							}
+//						}
+//						return res;
+//					});
+//					Collections.sort(crudTrabajador.getListado(), new Comparator<Trabajador>() {
+//						@Override
+//						public int compare(Trabajador t11, Trabajador t21) {
+//							// TODO Auto-generated method stub
+//							int res = 0;
+//							if (t11.calcularSueldo() > t21.calcularSueldo()) {
+//								res = 1;
+//							}else {
+//								if (t11.calcularSueldo() < t21.calcularSueldo()) {
+//									res = -1;
+//								}
+//							}
+//							return res;
+//						}
+//					});
+					
+					Consumer <Trabajador> cons = o1 -> {
+						System.out.printf("%s tiene como dni: %s, trabaja %d horas y gana %.2f €, ya que la hora la cobra a %.2f €", o1.getNombre(), o1.getDni(), o1.getHorasTrabajadas()
+								, o1.calcularSueldo(), o1.getSueldoPorHora());
+					};
+					
+					
 					crudTrabajador.mostrarLista();
 					break;
 				default:
@@ -97,5 +150,6 @@ public class Principal {
 		System.out.println("Pulse 4 para mostrar los trabajadores ordenados por el sueldo (ascendente)");
 		System.out.println("Pulse 0 para salir");
 	}
+	
 
 }
